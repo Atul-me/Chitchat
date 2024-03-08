304f3a9964346cad019224a68e9de947cafdb4d8
Atul-me <atulkur1999@gmail.com>
2024-03-08 20:38:05 +0530

    3

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+const connectDB = async() => {
+    try{
+        const con = await mongoose.connect(process.env.DATABASE_URL);
+
+        console.log(`MongoDB Connected: ${con.connection.host}`);
+    }
+    catch(error){
+        console.log(`Error: ${error.message}`);
+        process.exit();
+    }
+}
+
+module.exports = connectDB;
